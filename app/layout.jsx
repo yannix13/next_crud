902995709804422a83912189518752225fcab063
yannis staali <yannis@MacBook-Air-de-yannis.local>
2023-10-06 21:39:11 +0200

    sign in module finis

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css';
 
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
 export const metadata = {
     title: "Promptopia",
     description: 'Discover & share AI Prompts'
@@ -9,13 +12,16 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
         <body>
-            <div className='main'>
-                <div className='gradient'></div>
-            </div>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient'></div>
+                </div>
 
-            <main className='app'>
-                {children}
-            </main>
+                <main className='app'>
+                    <Nav/>
+                    {children}
+                </main>
+            </Provider>
         </body>
     </html>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
@@ -14,7 +14,7 @@ const Home = () => {
         and share creative prompts
       </p>
 
-      {/* Feed */}
+      <Feed/>
     </section>
   )
 }
