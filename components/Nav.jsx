a494427d4b3b26e6c48a18f2baf2c4bd8baf7c00
yannis staali <yannis@MacBook-Air-de-yannis.local>
2023-10-09 19:27:01 +0200

    Done with create prompt module

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,20 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  //const isUserLoggedIn = true;
+  const {data: session} = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   }, [])
 
   return (
@@ -35,7 +37,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        { isUserLoggedIn ? (
+        { session?.user ? (
           <div className="flex gap-3 md:gap-5">
               <Link href="/create-prompt"
               className="black_btn"
@@ -49,10 +51,10 @@ const Nav = () => {
 
               <Link href="/profile">
                 <Image
-                  src="/assets/images/logo.svg"
+                  src={session?.user.image}
                   width={37}
                   height={37}
-                  className="rounded-full"
+                  className="rounded-full cursor-pointer"
                   alt="profile"
                 />
               </Link>
@@ -76,13 +78,13 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ?  (
+        {session?.user ?  (
           <div className="flex">
              <Image
-                  src="/assets/images/logo.svg"
+                  src={session?.user.image}
                   width={37}
                   height={37}
-                  className="rounded-full"
+                  className="rounded-full cursor-pointer"
                   alt="profile"
                   onClick={() => setToggleDropdown((prev) => ! prev)}
                 />
